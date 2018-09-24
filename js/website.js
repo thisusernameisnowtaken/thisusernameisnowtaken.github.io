fe45dc390b60cffc17844346b6934abf1ba2534a
thisusernameisnowtaken <thisusernameisnowtaken@users.noreply.github.com>
2018-09-24 11:50:41 +0100

    New slideshow page
    Added a new page with a slideshow

--- js/website.js
+++ js/website.js
@@ -1,1 +1,28 @@
- 
+var slideindex = 1;
+showSlides(slideindex);
+
+//next/previous
+function changeSlides(n) {
+    showSlides(slideindex +=n)    
+}
+//thumbnail image controls
+function currentSlide(n){
+    showSlides(slideIndex = n)    
+}
+
+function showSlides(n){
+var i;
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+}
+for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+} 
+
